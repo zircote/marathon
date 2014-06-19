7ec122245b95513ffb40f8109edc9ea8b38249aa
mlunoe <michael.lunoe@gmail.com>
2014-06-18 20:58:48 -0700

    Moving app configuration to its own component

--- src/main/resources/assets/js/components/AppModalComponent.jsx
+++ src/main/resources/assets/js/components/AppModalComponent.jsx
@@ -85,50 +85,6 @@ define([
       var hasHealth =
         model.get("healthChecks") != null &&
         model.get("healthChecks").length > 0;
-      var cmdNode = (model.get("cmd") == null) ?
-        <dd className="text-muted">Unspecified</dd> :
-        <dd>{model.get("cmd")}</dd>;
-      var constraintsNode = (model.get("constraints").length < 1) ?
-        <dd className="text-muted">Unspecified</dd> :
-        model.get("constraints").map(function(c) {
-
-          // Only include constraint parts if they are not empty Strings. For
-          // example, a hostname uniqueness constraint looks like:
-          //
-          //     ["hostname", "UNIQUE", ""]
-          //
-          // it should print "hostname:UNIQUE" instead of "hostname:UNIQUE:", no
-          // trailing colon.
-          return (
-            <dd key={c}>
-              {c.filter(function(s) { return s !== ""; }).join(":")}
-            </dd>
-          );
-        });
-      var containerNode = (model.get("container") == null) ?
-        <dd className="text-muted">Unspecified</dd> :
-        <dd>{JSON.stringify(model.get("container"))}</dd>;
-      var envNode = (Object.keys(model.get("env")).length === 0) ?
-        <dd className="text-muted">Unspecified</dd> :
-
-        // Print environment variables as key value pairs like "key=value"
-        Object.keys(model.get("env")).map(function(k) {
-          return <dd key={k}>{k + "=" + model.get("env")[k]}</dd>
-        });
-      var executorNode = (model.get("executor") === "") ?
-        <dd className="text-muted">Unspecified</dd> :
-        <dd>{model.get("executor")}</dd>;
-      var portsNode = (model.get("ports").length === 0 ) ?
-        <dd className="text-muted">Unspecified</dd> :
-        <dd>{model.get("ports").join(",")}</dd>;
-      var versionNode = (model.get("version") == null) ?
-        <dd className="text-muted">Unspecified</dd> :
-        <dd>{model.get("version").toLocaleString()}</dd>;
-      var urisNode = (model.get("uris").length === 0) ?
-        <dd className="text-muted">Unspecified</dd> :
-        model.get("uris").map(function(u) {
-          return <dd key={u}>{u}</dd>;
-        });
 
       return (
         <ModalComponent ref="modalComponent" onDestroy={this.props.onDestroy}
@@ -184,30 +140,7 @@ define([
               </StackedViewComponent>
             </TabPaneComponent>
             <TabPaneComponent id="configuration">
-              <dl className="dl-horizontal">
-                <dt>Command</dt>
-                {cmdNode}
-                <dt>Constraints</dt>
-                {constraintsNode}
-                <dt>Container</dt>
-                {containerNode}
-                <dt>CPUs</dt>
-                <dd>{model.get("cpus")}</dd>
-                <dt>Environment</dt>
-                {envNode}
-                <dt>Executor</dt>
-                {executorNode}
-                <dt>Instances</dt>
-                <dd>{model.get("instances")}</dd>
-                <dt>Memory (MB)</dt>
-                <dd>{model.get("mem")}</dd>
-                <dt>Ports</dt>
-                {portsNode}
-                <dt>URIs</dt>
-                {urisNode}
-                <dt>Version</dt>
-                {versionNode}
-              </dl>
+              <AppConfigComponent model={model} />
             </TabPaneComponent>
           </TogglableTabsComponent>
         </ModalComponent>
